e9c0ce879248953d11663b9b22ee19323adf2bbe
aulkiller <58225261+aulkiller@users.noreply.github.com>
2020-08-04 01:11:26 +0700

    p

--- JSFolder/main.js
+++ JSFolder/main.js
@@ -1,18 +1,20 @@
 const navSlide = () => {
     const menubtn = document.querySelector('.menu-btn');
     const nav = document.querySelector('.nav-items');
+    const navbar = document.querySelector('header');
     const navitems = document.querySelectorAll('.nav-items li')
 
     //Navtoggle
     menubtn.addEventListener('click', () =>{
         nav.classList.toggle('nav-active');
+        navbar.classList.toggle('nav-test');
 
         //animasi persatua
         navitems.forEach((items,index) => {
             if(items.style.animation){
                 items.style.animation = '';
             } else {
-                items.style.animation = `navitemsFade 0.5s ease forwards ${index / 7 + 1}s`;
+                items.style.animation = `navitemsFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
     });
